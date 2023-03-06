a4c47f1de9619eb5160f40917cfd0119769623c1
Sam Der <sder@uci.edu>
2023-03-06 10:04:06 -0800

    Fix frontend code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hack @ UCI Tech Deliverable</h1>
-      <input></input>
-      <button>Submit</button>
-      <div>
+      <form>
+        <input type="text" />
+        <button type="submit">Submit</button>
+      </form>
+      <div className="messages">
         <h3>Test User</h3>
         <p>test message</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
